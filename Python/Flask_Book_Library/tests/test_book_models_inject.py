1f00f9396208dbb81f21e431d626ac78e97d6b2f
cyberorzech <natan.orze@gmail.com>
2023-12-11 08:41:58 -0600

    Add yield fixture to tests

--- Python/Flask_Book_Library/tests/test_book_models_inject.py
+++ Python/Flask_Book_Library/tests/test_book_models_inject.py
@@ -1,14 +1,14 @@
-import pytest
+# import pytest
 
-from project.books.models import Book
+# from project.books.models import Book
 
-class Test_Book_Models_Extreme:
-    @pytest.fixture
-    def asd(self):
-        return 1
+# class Test_Book_Models_Extreme:
+#     @pytest.fixture
+#     def asd(self):
+#         return 1
 
-    def test_x(self, asd):
-        assert 1 == asd
+#     def test_x(self, asd):
+#         assert 1 == asd
 
-if __name__ == "__main__":
-    raise NotImplementedError("Use run_tests.bash")
+# if __name__ == "__main__":
+#     raise NotImplementedError("Use run_tests.bash")
--- Python/Flask_Book_Library/tests/test_book_models_valid.py
+++ Python/Flask_Book_Library/tests/test_book_models_valid.py
@@ -4,11 +4,36 @@ from project.books.models import Book
 
 class Test_Book_Models_Extreme:
     @pytest.fixture
-    def asd(self):
-        return 1
+    def example_valid_data(self):
+        return {
+            "name": "The Great Book",
+            "author": "John Doe",
+            "year_published": 1998,
+            "book_type": "poem",
+            "status": "not available"
+        }
 
-    def test_x(self, asd):
-        assert 1 == asd
+    x = ["1", "2", "3"]
+
+    @pytest.fixture(params=x)
+    def get_random_string(self, request):
+        yield request.param
+
+    def test_example_valid_data(self, example_valid_data):
+        x = Book(**example_valid_data)
+        assert x.author == "John Doe"
+
+    def test_name(self, example_valid_data, get_random_string):
+        _, author, year_published, book_type, status = example_valid_data.values()
+        for _ in range(0, 5):
+            x = Book(
+                name='ok',
+                author=author,
+                year_published=year_published,
+                book_type=book_type,
+                status=status
+            )
+            assert isinstance(get_random_string, str)
 
 if __name__ == "__main__":
     raise NotImplementedError("Use run_tests.bash")
